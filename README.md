c7133aaac1ae35de645da98922680eef96acca48
ChaoticByte <contact@chaoticbyte.net>
2026-02-14 20:45:02 +0100

    Initial commit with existing project files

---
+++ README.md
@@ -0,0 +1,57 @@
+# Image based Load Remover for LibreSplit
+
+This is an experimental image-based Load Remover compatible with LibreSplit.
+
+
+## Notes
+
+- This works only in games where a distinct region, e.g. a text is present at exactly the same position during the loading screen
+- Currently fixed detection framerate of 30fps
+- Wayland is not supported!
+
+
+## Compatibility
+
+### System Requirements
+
+- Tested on Ubuntu 24.04
+- Python 3.12
+- X11
+
+You also need the pip packages in `requirements.txt`!
+
+### Games
+
+You may have to write your own profile. Or tweak existing ones when you have another screen resolution or ingame settings.
+
+
+## Create a profile
+
+Take a look at the profile for Horizon Zero Dawn:
+
+```yml
+# Path to the reference image, relative to the profile file:
+reference: ./hzd-loading.png
+# starts with 1
+monitor: 1
+# The region containing something that is always
+# there in the loading screen - this should be
+# as distinct as possible to mitigate false-positives
+region:
+  left: 98
+  top: 975
+  width: 118
+  height: 25
+# How to calculate the difference of the
+# current frame against the reference
+difference:
+  # The only existing method currently is
+  # nrmse (Normalized Root Mean Square Error)
+  method: nrmse
+  # If the difference < this threshold, the current
+  # frame is considered a loading screen
+  threshold: 0.02
+```
+
+You can dump cropped screenshots in 1-second intervals using the `dump-images` command, and use one of those as the reference image.
+Run the `dump-difference` command to debug the differences for every frame.
---
+++ ilr.py
@@ -0,0 +1,194 @@
+#!/usr/bin/env python3
+
+from argparse import ArgumentParser
+from os import environ, getuid
+from pathlib import Path
+from socket import socket, AF_UNIX, SOCK_STREAM
+from struct import pack
+from sys import stderr
+from time import time, sleep
+
+import numpy as np
+from mss import mss
+from PIL import Image
+from yaml import safe_load as yaml_safe_load
+from skimage.metrics import normalized_root_mse
+
+
+# Screenshot interval
+
+INTERVAL_DETECT = 1.0 / 30.0
+INTERVAL_DUMP = 1.0
+
+
+# Methods for calculating the difference between reference image an current screenshot
+
+METHOD_NRMSE = "nrmse"
+METHODS = [
+    METHOD_NRMSE
+]
+
+#
+
+class Profile:
+    
+    def __init__(
+        self,
+        reference_image: Path,
+        monitor: int,
+        left: int, top: int,
+        width: int, height: int,
+        difference_method: str,
+        difference_threshold: float,
+        profile_yml_file: Path = None
+    ):
+        if not isinstance(reference_image, Path):
+            raise TypeError("reference_image must be Path")
+        self.reference_image = reference_image
+        self.monitor = int(monitor)
+        self.left = int(left)
+        self.top = int(top)
+        self.width = int(width)
+        self.height = int(height)
+        if not difference_method in METHODS:
+            raise ValueError(f"Unknown method '{difference_method}' - supported values: {' '.join(METHODS)}")
+        self.difference_method = difference_method
+        self.diff_threshold = float(difference_threshold)
+        self.profile_yml_file = profile_yml_file
+
+    @classmethod
+    def from_yml_file(cls, filepath: Path):
+        profile_dict = yaml_safe_load(filepath.read_text())
+        return cls(
+            Path(filepath.absolute().parent / Path(profile_dict["reference"])),
+            profile_dict["monitor"],
+            profile_dict["region"]["left"],
+            profile_dict["region"]["top"],
+            profile_dict["region"]["width"],
+            profile_dict["region"]["height"],
+            profile_dict["difference"]["method"],
+            profile_dict["difference"]["threshold"],
+            profile_yml_file=filepath
+        )
+
+# Functions for the LibreSplit part
+
+LIBRESPLIT_CMD_START_SPLIT = 0
+LIBRESPLIT_CMD_STOP_RESET = 1
+
+def get_xdg_runtime_dir() -> Path:
+    if "XDG_RUNTIME_DIR" in environ:
+        return Path(environ["XDG_RUNTIME_DIR"]).absolute()
+    else:
+        return Path(f"/run/user/{getuid}")
+
+def get_libresplit_socket_file() -> Path:
+    return get_xdg_runtime_dir() / "libresplit.sock"
+
+def libresplit_ctl(cmd: int, address: Path):
+    with socket(AF_UNIX, SOCK_STREAM) as sock:
+        try:
+            cmd_payload = pack("<I", cmd)
+            sock.connect(str(address.absolute().resolve()))
+            sock.sendall(pack(">I", len(cmd_payload)) + cmd_payload)
+        except ConnectionRefusedError as err:
+            print(f"Couldn't send command to {str(address)}", file=stderr)
+            print(err.with_traceback(None)) # ?
+
+
+# Calculate difference -> match reference
+
+def match_reference(reference, current, profile: Profile) -> tuple[bool, float]:
+    if profile.difference_method == METHOD_NRMSE:
+        diff = normalized_root_mse(reference, current)
+        return diff < profile.diff_threshold, diff
+    # elif ...
+
+
+# image-related helper functions
+
+def remove_alpha(img):
+    return np.delete(img, 3, axis=2) # alpha has idx 3
+
+
+def grab(mss_instance, monitor, profile: Profile):
+    return mss_instance.grab({
+                "left": monitor["left"] + profile.left,
+                "top": monitor["top"] + profile.top,
+                "width": profile.width,
+                "height": profile.height
+            })
+
+def grab_array_noalpha(mss_instance, monitor, profile):
+    return remove_alpha(np.array(grab(mss_instance, monitor, profile)))
+
+
+# main functions
+
+def run(profile: Profile, dump_diff_only: bool = False):
+    ref = Image.open(profile.reference_image, formats=["png"])
+    ref.load()
+    ref = np.asarray(ref)[:, :, ::-1] # RGB to BGR
+    state_loading = False
+    libresplit_socket_file = get_libresplit_socket_file()
+    with mss() as ms:
+        mon = ms.monitors[profile.monitor]
+        t1 = time()
+        while True:
+            current = grab_array_noalpha(ms, mon, profile)
+            is_match, diff = match_reference(ref, current, profile)
+            if dump_diff_only:
+                print(diff)
+            else:
+                if is_match:
+                    if not state_loading:
+                        state_loading = True
+                        print("loading")
+                        libresplit_ctl(LIBRESPLIT_CMD_STOP_RESET, libresplit_socket_file)
+                else:
+                    if state_loading:
+                        state_loading = False
+                        print("not loading")
+                        libresplit_ctl(LIBRESPLIT_CMD_START_SPLIT, libresplit_socket_file)
+            dt = time() - t1
+            sleep(max(0, INTERVAL_DETECT - dt))
+            t1 = time()
+
+
+def dumpimgs(profile: Profile):
+    if profile.profile_yml_file is None:
+        out_dir = Path(f"dump_{int(time())}")
+    else:
+        out_dir = profile.profile_yml_file.absolute().parent / f"dump_{int(time())}"
+    out_dir.mkdir()
+    with mss() as ms:
+        mon = ms.monitors[profile.monitor]
+        while True:
+            sx = grab(ms, mon, profile)
+            img = Image.frombytes("RGB", sx.size, sx.bgra, "raw", "BGRX")
+            img.save(out_dir / f"{int(time() * 10)}.png", format="png")
+            # we don't need to use dt here, because the added precision is negligible here
+            sleep(INTERVAL_DUMP)
+
+
+# entrypoint
+
+CMD_RUN = "run"
+CMD_DUMPDIFF = "dump-difference"
+CMD_DUMPIMAGES = "dump-images"
+
+if __name__ == "__main__":
+    argparser = ArgumentParser()
+    argparser.add_argument("command", type=str, choices=[CMD_RUN, CMD_DUMPDIFF, CMD_DUMPIMAGES], help="command")
+    argparser.add_argument("profile", type=Path, help="Path to the profile configuration")
+    args = argparser.parse_args()
+    # parse profile
+    p = Profile.from_yml_file(args.profile)
+    # command
+    cmd = args.command
+    if cmd == CMD_RUN:
+        run(p)
+    elif cmd == CMD_DUMPDIFF:
+        run(p, dump_diff_only=True)
+    elif cmd == CMD_DUMPIMAGES:
+        dumpimgs(p)
